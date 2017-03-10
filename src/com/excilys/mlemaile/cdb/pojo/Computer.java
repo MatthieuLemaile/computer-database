fc2a1866ba6c73a2c56e4dc7e169de232d193fdd
Matthieu Lemaile <mlemaile@excilys.com>
2017-03-10 16:44:25 +0100

    Ajout des POJOs

---
+++ src/com/excilys/mlemaile/cdb/pojo/Computer.java
@@ -0,0 +1,46 @@
+package com.excilys.mlemaile.cdb.pojo;
+
+import java.time.LocalDate;
+
+public class Computer {
+	private int id;
+	private String name;
+	private LocalDate introduced;
+	private LocalDate discontinued;
+	private int company_id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public LocalDate getIntroduced() {
+		return introduced;
+	}
+	public void setIntroduced(LocalDate introduced) {
+		this.introduced = introduced;
+	}
+	public LocalDate getDiscontinued() {
+		return discontinued;
+	}
+	public void setDiscontinued(LocalDate discontinued) {
+		//Pour pouvoir remettre à zéro cette propriété
+		if(discontinued==null){
+			this.discontinued = discontinued;
+		}else if(introduced!=null && discontinued.isAfter(introduced)){
+			this.discontinued = discontinued;
+		}
+	}
+	public int getCompany_id() {
+		return company_id;
+	}
+	public void setCompany_id(int company_id) {
+		this.company_id = company_id;
+	} 
+}

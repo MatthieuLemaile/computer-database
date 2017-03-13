27553d9f329ef6e8755b567c8f2b615ad3c0b26a
MatthieuLemaile <matlem35@yahoo.fr>
2017-03-13 17:23:45 +0100

    ajout test connection à la db

---
+++ src/com/excilys/mlemaile/cdb/test/persistence/DatabaseconnectionTest.java
@@ -0,0 +1,17 @@
+package com.excilys.mlemaile.cdb.test.persistence;
+
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import com.excilys.mlemaile.cdb.persistence.DatabaseConnection;
+
+public class DatabaseconnectionTest {
+
+	@Test
+	public void test() {
+		assertNotNull("failed to connect, to the database",DatabaseConnection.getConnection());
+	}
+
+}

2f4b90e4a4ea439b43c58b9881c9137eaf2da24d
MatthieuLemaile <matlem35@yahoo.fr>
2017-03-14 14:26:48 +0100

    Connexion à la DB dans un properties

--- src/main/java/com/excilys/mlemaile/cdb/persistence/DatabaseConnection.java
+++ src/main/java/com/excilys/mlemaile/cdb/persistence/DatabaseConnection.java
@@ -4,6 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.FileBasedConfiguration;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.FileBasedConfigurationBuilder;
+import org.apache.commons.configuration2.builder.fluent.Parameters;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
 /**
  * This class create the connection to the database
  * @author Matthieu Lemaile
@@ -18,15 +25,28 @@ public class DatabaseConnection {
 	 */
 	public static Connection getConnection(){
 		if(dbConnection==null){
-			try {
-				Class.forName("com.mysql.jdbc.Driver");
-				dbConnection = DriverManager
-                        .getConnection("jdbc:mysql://localhost/computer-database-db?"
-                                        + "user=admincdb&password=TJeWkg5equo1mpOIWVUI"
-                                        + "&zeroDateTimeBehavior=convertToNull");
-			} catch (ClassNotFoundException | SQLException e) {
-				e.printStackTrace();
-			}
+				try {
+					Parameters params = new Parameters();
+					FileBasedConfigurationBuilder<FileBasedConfiguration> builder =
+							new FileBasedConfigurationBuilder<FileBasedConfiguration>(PropertiesConfiguration.class)
+							.configure(params.properties().setFileName("src/main/resources/database.properties"));
+					Configuration config = builder.getConfiguration();
+					String startUrl =config.getString("start-url");
+					String database =config.getString("database");
+					String user = config.getString("user");
+					String password = config.getString("password");
+					String zeroDataTimeBehavior = config.getString("zeroDateTimeBehavior");
+					Class.forName("com.mysql.jdbc.Driver");
+					
+					dbConnection = DriverManager
+					        .getConnection(startUrl+database+"?user="+user
+					                        + "&password="+password
+					                        + "&zeroDateTimeBehavior="+zeroDataTimeBehavior);
+				} catch (SQLException | ClassNotFoundException | ConfigurationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
 		}
 		return dbConnection;
 	}
@@ -38,6 +58,7 @@ public class DatabaseConnection {
 		try {
 			if(dbConnection!=null && !dbConnection.isClosed()){
 				dbConnection.close();
+				dbConnection = null;
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();

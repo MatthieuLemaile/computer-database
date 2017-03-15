22b6811ff88cf4358b06dce38d18327fe3ef1437
MatthieuLemaile <matlem35@yahoo.fr>
2017-03-15 17:23:50 +0100

    Refactoring test to comply to the new implementation

--- src/test/com/excilys/mlemaile/cdb/model/TestCompany.java
+++ src/test/com/excilys/mlemaile/cdb/model/TestCompany.java
@@ -9,11 +9,9 @@ import com.excilys.mlemaile.cdb.model.Company;
 public class TestCompany {
 	@Test
 	public void testCompany(){
-		Company company = new Company();
 		int id = 4;
-		company.setId(id);
 		String name = "Company's name";
-		company.setName(name);
+		Company company = new Company.Builder().name(name).id(id).build();
 		assertEquals("The company id is not properly set",id,company.getId());
 		assertEquals("The company name is not properly set",name, company.getName());
 	}
--- src/test/com/excilys/mlemaile/cdb/model/TestComputer.java
+++ src/test/com/excilys/mlemaile/cdb/model/TestComputer.java
@@ -12,43 +12,40 @@ import com.excilys.mlemaile.cdb.model.Computer;
 public class TestComputer {
 	@Test
 	public void testCreationComputer(){
-		Computer computer = new Computer("");
+		Computer computer = new Computer.Builder("").build();
 		assertNotNull(computer);
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void testCreationComputerIllegalArgument(){
-		Computer computer = new Computer("");
+		Computer computer = new Computer.Builder("").build();
 		computer.setId(-3);
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void testCreationComputerIllegalArgument0(){
-		Computer computer = new Computer("");
+		Computer computer = new Computer.Builder("").build();
 		computer.setId(0);
 	}
 	
 	@Test
 	public void testComputer(){
 		String name = "name of the computer";
-		Computer computer = new Computer(name);
-		computer.setId(5);
 		LocalDate introduced = LocalDate.now();
 		introduced.minusYears(1);
-		computer.setIntroduced(introduced);
+		LocalDate discontinued = introduced.plusMonths(4);
+		Company company = new Company.Builder().id(8).build();
+		
+		Computer computer = new Computer.Builder(name).id(5).introduced(introduced).discontinued(discontinued).company(company).build();
 		assertEquals("The introduced date is not set properly",introduced,computer.getIntroduced());
 		assertEquals("The name is not set properly",name,computer.getName());
-		int company_id = 8;
-		computer.setCompany_id(company_id);
-		assertEquals("The company id is not set properly",company_id,computer.getCompany_id());
-		LocalDate discontinued = introduced.plusMonths(4);
-		computer.setDiscontinued(discontinued);
+		assertEquals("The company id is not set properly",company,computer.getCompany());
 		assertEquals("The discontinued date is not set properly",discontinued,computer.getDiscontinued());
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void testComputerDiscontinuedBeforeIntroduced(){
-		Computer computer = new Computer("");
+		Computer computer = new Computer.Builder("").build();
 		LocalDate introduced = LocalDate.now();
 		introduced.minusYears(1);
 		LocalDate discontinued = introduced.minusMonths(4);
